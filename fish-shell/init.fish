72a0e55b235b6de3dfed01fe17b21e6be365e430
LEGOlord208 <LEGOlord208@krake.one>
2017-05-31 21:35:56 +0200

    Fish Shell: Revert stderred

--- fish-shell/init.fish
+++ fish-shell/init.fish
@@ -1,7 +1,5 @@
 function fish_greeting
 end
 
-# stderred
-bass export LD_PRELOAD="$HOME/.stderred/stderred/build/libstderred.so\${LD_PRELOAD:+:$LD_PRELOAD}"
 # Dircolors
 bass eval `dircolors ~/.dir_colors/dircolors`
