e1bf727fbff6e5cee42bd579dec82ed13e42a83b
LEGOlord208 <LEGOlord208@krake.one>
2017-05-31 20:48:01 +0200

    Fish Shell: stderred

--- fish-shell/init.fish
+++ fish-shell/init.fish
@@ -1,5 +1,7 @@
 function fish_greeting
 end
 
+# stderred
+bass export LD_PRELOAD="$HOME/.stderred/stderred/build/libstderred.so\${LD_PRELOAD:+:$LD_PRELOAD}"
 # Dircolors
 bass eval `dircolors ~/.dir_colors/dircolors`
