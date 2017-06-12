1121fc8fa3788a38de5fde7c12a84c0b5bd2b9c4
LEGOlord208 <LEGOlord208@krake.one>
2017-06-12 14:40:28 +0200

    Fish Shell: RVM

--- fish-shell/init.fish
+++ fish-shell/init.fish
@@ -1,6 +1,9 @@
-# Dircolors
-bass eval `dircolors ~/.dir_colors/dircolors`
-
 # Aliases
 alias rm="rmtrash"
 alias rmdir="rmdirtrash"
+
+# Default Ruby
+rvm default
+
+# Dircolors
+bass eval `dircolors ~/.dir_colors/dircolors`
