5a8dcde63f2b10d2ab53cbc8827ba61beb26b08c
LEGOlord208 <LEGOlord208@krake.one>
2017-06-12 21:16:07 +0200

    Fish Shell: Added 'loop' function

--- fish-shell/init.fish
+++ fish-shell/init.fish
@@ -2,12 +2,23 @@
 alias rm="rmtrash"
 alias rmdir="rmdirtrash"
 
-function unicopy --description "unicopy <character>"
-	if test -z $argv[1]
+function unicopy --description "unicopy <character>" --argument character
+	if test -z $character
 		echo "unicopy <character>"
 		return
 	end
-	unicode $argv[1] --format "{pchar}" | xclip -sel clip
+	unicode $character --format "{pchar}" | xclip -sel clip
+end
+
+function loop --description "loop <n> <command>" --argument n command
+	if begin test -z $n; or test -z $command; end
+		echo "loop <n> <command>"
+		return
+	end
+
+	for _ in (seq 1 $n)
+		eval $argv[2..-1]
+	end
 end
 
 # Default Ruby
