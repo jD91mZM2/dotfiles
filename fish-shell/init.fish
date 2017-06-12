a06c00c59acbcfd1d345c0334deaaaf76866ac24
LEGOlord208 <LEGOlord208@krake.one>
2017-06-12 20:38:24 +0200

    Fish Shell: unicopy function

--- fish-shell/init.fish
+++ fish-shell/init.fish
@@ -2,6 +2,14 @@
 alias rm="rmtrash"
 alias rmdir="rmdirtrash"
 
+function unicopy --description "unicopy <character>"
+	if test -z $argv[1]
+		echo "unicopy <character>"
+		return
+	end
+	unicode $argv[1] --format "{pchar}" | xclip -sel clip
+end
+
 # Default Ruby
 rvm default
 
