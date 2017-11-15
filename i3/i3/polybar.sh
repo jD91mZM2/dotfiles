d84acec1f04547af26f294ee1845b623e0110768
jD91mZM2 <me@krake.one>
2017-11-15 14:37:15 +0100

    polybar: More reliable system tray placement

--- i3/i3/polybar.sh
+++ i3/i3/polybar.sh
@@ -2,9 +2,10 @@
 
 killall polybar
 
-for monitor in $(xrandr | grep "\bconnected\b" | grep -v "primary" | cut -d ' ' -f 1); do
+# Primary first because system tray
+MONITOR="$(xrandr | grep "\bprimary\b" | cut -d ' ' -f 1)" polybar example & disown
+sleep 2
+
+for monitor in $(xrandr | grep "\bconnected\b" | grep -v "\bprimary\b" | cut -d ' ' -f 1); do
     MONITOR="$monitor" polybar example & disown
 done
-
-MONITOR="$(xrandr | grep "primary" | cut -d ' ' -f 1)" polybar example & disown
-# Primary last because traybar
