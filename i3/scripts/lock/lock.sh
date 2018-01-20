d5ce5fae1e5dcb345742f820df27eb9c111c91d9
jD91mZM2 <me@krake.one>
2018-01-20 10:17:13 +0100

    i3lock: 12-hour clock

--- i3/scripts/lock/lock.sh
+++ i3/scripts/lock/lock.sh
@@ -3,7 +3,14 @@
 # Pause notifications
 pkill dunst -USR1
 
-i3lock -knc "#111111" --timecolor "#FFFFFFFF" --textcolor "#FFFFFFFF" --datecolor "#FFFFFFFF"
+i3lock \
+    --clock \
+    --nofork \
+    --color     "#111111" \
+    --datecolor "#FFFFFFFF" \
+    --textcolor "#FFFFFFFF" \
+    --timecolor "#FFFFFFFF" \
+    --timestr   "%I:%M:%S"
 
 # Start notifications
 pkill dunst -USR2
