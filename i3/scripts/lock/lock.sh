1cec6f7f49c8924eb25a7404f466444d6d14c492
jD91mZM2 <me@krake.one>
2018-01-21 15:54:55 +0100

    i3lock: AM/PM indicator

--- i3/scripts/lock/lock.sh
+++ i3/scripts/lock/lock.sh
@@ -6,11 +6,12 @@ pkill dunst -USR1
 i3lock \
     --clock \
     --nofork \
+    --timesize 24 \
     --color     "#111111" \
     --datecolor "#FFFFFFFF" \
     --textcolor "#FFFFFFFF" \
     --timecolor "#FFFFFFFF" \
-    --timestr   "%I:%M:%S"
+    --timestr   "%I:%M:%S%p"
 
 # Start notifications
 pkill dunst -USR2
