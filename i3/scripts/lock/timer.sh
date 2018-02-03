4edd3b33842d1bdff12d433910703b0875f44d34
jD91mZM2 <me@krake.one>
2018-02-03 11:38:04 +0100

    xidlehook: Not when fullscreen

--- i3/scripts/lock/timer.sh
+++ i3/scripts/lock/timer.sh
@@ -7,4 +7,5 @@ xidlehook \
   --timer "$dir/lock.sh" \
   --notify 10 \
   --notifier  'xrandr --output "$(xrandr | grep primary | cut -d " " -f 1)" --brightness .1' \
-  --canceller 'xrandr --output "$(xrandr | grep primary | cut -d " " -f 1)" --brightness 1'
+  --canceller 'xrandr --output "$(xrandr | grep primary | cut -d " " -f 1)" --brightness 1' \
+  --not-when-fullscreen
