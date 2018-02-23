f167e33cd1188cca0cc3f526de57d458d3890ae2
jD91mZM2 <me@krake.one>
2018-02-23 18:37:37 +0100

    Add nm-applet to startup applications

--- xmonad/startup.sh
+++ xmonad/startup.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
-# Background
-
-compton -b
-udiskie &
-~/.dotfiles/lock-timer.sh slock &
+# Setup
 
 ~/.screenlayout/default.sh
 xsetroot -cursor_name left_ptr
 xinput disable "$(xinput list | grep Touchpad | cut -d '=' -f 2 | cut -d $'\t' -f 1)"
 feh --bg-fill /usr/share/archlinux/wallpaper/archlinux-firestarter.jpg
 # Note: feh has to be after screenlayout
+
+# Background
+
+compton -b
+udiskie &
+~/.dotfiles/lock-timer.sh slock &
 
 # Interactive
 
@@ -20,5 +22,6 @@ dropbox &
 evolution &
 konsole &
 mattermost-desktop &
+nm-applet &
 xfce4-panel &
 xfce4-power-manager &
