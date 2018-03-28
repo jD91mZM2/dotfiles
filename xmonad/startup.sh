a005c20f64ac40d646c7535a2d10780fdcfba109
jD91mZM2 <me@krake.one>
2018-03-28 21:07:12 +0200

    xmonad: .fehbg

--- xmonad/startup.sh
+++ xmonad/startup.sh
@@ -5,7 +5,7 @@
 ~/.screenlayout/default.sh
 xsetroot -cursor_name left_ptr
 xinput disable "$(xinput list | grep Touchpad | cut -d '=' -f 2 | cut -d $'\t' -f 1)"
-feh --bg-fill /usr/share/archlinux/wallpaper/archlinux-firestarter.jpg
+~/.fehbg
 # Note: feh has to be after screenlayout
 
 # Background
