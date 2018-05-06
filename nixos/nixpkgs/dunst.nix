44e3c4f1f131d8a22638775c04f4ff188db54bd4
jD91mZM2 <me@krake.one>
2018-05-06 21:08:48 +0200

    Use home-manager

---
+++ nixos/nixpkgs/dunst.nix
@@ -0,0 +1,61 @@
+{ pkgs, ... }:
+{
+  enable = true;
+  iconTheme = {
+    name = "Numix-Circle";
+    package = pkgs.numix-icon-theme-circle;
+  };
+  settings = {
+    global = {
+      geometry = "300x5-30+20";
+      indicate_hidden = true;
+      transparency = 20;
+      separator_height = 2;
+      padding = 8;
+      horizontal_padding = 8;
+      frame_width = 3;
+      frame_color = "#aaaaaa";
+      separator_color = "frame";
+      sort = true;
+      idle_threshold = 120;
+      font = "Hack 8";
+      markup = "full";
+      format = "<b>%s</b>\\n%b";
+      alignment = "left";
+      show_age_threshold = 60;
+      word_wrap = true;
+      stack_duplicates = true;
+      show_indicators = true;
+      icon_position = "left";
+      max_icon_size = 32;
+      icon_path = "/usr/share/icons/gnome/16x16/status/:/usr/share/icons/gnome/16x16/devices/";
+      sticky_history = true;
+      history_length = 5;
+    };
+    shortcuts = {
+      close = "ctrl+space";
+      close_all = "ctrl+shift+space";
+      history = "ctrl+grave";
+      context = "ctrl+shift+period";
+    };
+
+    urgency_low = {
+      background = "#101010";
+      foreground = "#ffffff";
+      frame_color = "#101010";
+      timeout = 10;
+    };
+    urgency_normal = {
+      background = "#000000";
+      foreground = "#ffffff";
+      frame_color = "#000000";
+      timeout = 10;
+    };
+    urgency_critical = {
+      background = "#900000";
+      foreground = "#ffffff";
+      frame_color = "#900000";
+      timeout = 0;
+    };
+  };
+}
---
+++ nixos/nixpkgs/home.nix
@@ -0,0 +1,97 @@
+{ pkgs, ... }:
+
+let
+  dotfiles = "${builtins.getEnv "HOME"}/.dotfiles";
+  aliases = {
+    clear = "clear; echo -ne \"\\e[3J\"";
+    git = "hub";
+    ls = "ls -CF --color=auto";
+    nix-shell = "nix-shell --run zsh";
+    rsynca = "rsync -avzhP";
+  };
+in
+{
+  # Imports
+  services.dunst = (import ./dunst.nix { inherit pkgs; });
+
+  programs.home-manager = {
+    enable = true;
+    path = https://github.com/rycee/home-manager/archive/master.tar.gz;
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "jD91mZM2";
+    userEmail = "me@krake.one";
+
+    signing = {
+      key = "BC5DAE4EC168B1F9B94C98503055D54729A72666";
+      signByDefault = true;
+    };
+    extraConfig = ''
+      [pull]
+      rebase = true;
+      [diff]
+      tool = nvimdifftool
+      [difftool "nvimdifftool"]
+      cmd = "nvim -d \"$LOCAL\" \"$REMOTE\""
+      [merge]
+      tool = nvimdifftool
+      [mergetool "nvimdifftool"]
+      cmd = "nvim -d \"$LOCAL\" \"$REMOTE\""
+    '';
+  };
+
+  programs.bash = {
+    enable = true;
+    profileExtra = (builtins.readFile "${dotfiles}/.profile");
+    initExtra = ''
+      eval "$(dircolors ~/.dircolors)"
+
+      powerline() {
+          PS1="$(powerline-rs --shell bash $?)"
+      }
+      PROMPT_COMMAND=powerline
+    '';
+  };
+  programs.zsh = {
+    enable = true;
+    shellAliases = aliases;
+    initExtra = ''
+      eval "$(dircolors ~/.dircolors)"
+
+      powerline() {
+          PS1="$(powerline-rs --shell zsh $?)"
+      }
+      precmd_functions+=(powerline)
+
+      export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="bg=10"
+    '';
+  };
+
+  gtk = {
+    enable = true;
+    font = {
+      name = "Cantarell 11";
+      package = pkgs.cantarell-fonts;
+    };
+    iconTheme = {
+      name = "Numix-Circle";
+      package = pkgs.numix-icon-theme-circle;
+    };
+    theme = {
+      name = "Adapta-Nokto-Eta";
+      package = pkgs.adapta-gtk-theme;
+    };
+  };
+
+  services.compton = {
+    enable = true;
+    fade = true;
+    fadeDelta = 5;
+    extraOptions = ''
+      inactive-dim = 0.05;
+      mark-ovredir-focused = false; # causes issues in xmonad
+    '';
+  };
+}
