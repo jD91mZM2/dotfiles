290412b94efd84cc4b2a2890ecfafc821756549e
jD91mZM2 <me@krake.one>
2018-05-07 07:01:50 +0200

    Use compton from systemd (generated by home-manager)

--- xmonad/startup.sh
+++ xmonad/startup.sh
@@ -2,15 +2,16 @@
 
 # Setup
 
-~/.screenlayout/default.sh
 xsetroot -cursor_name left_ptr
 xinput disable "$(xinput list | grep Touchpad | cut -d '=' -f 2 | cut -d $'\t' -f 1)"
+
+~/.screenlayout/default.sh
 ~/.fehbg
 # Note: feh has to be after screenlayout
 
 # Background
 
-compton -b
+systemctl start --user compton
 udiskie &
 ~/.dotfiles/lock-timer.sh slock &
 
