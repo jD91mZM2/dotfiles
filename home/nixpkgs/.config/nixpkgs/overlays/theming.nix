0a88d5542188ec1aa2d76b0a6851ffb9cd29c9b9
jD91mZM2 <me@krake.one>
2020-05-04 20:19:26 +0200

    NixOS: Temporary build-fail workaround

--- home/nixpkgs/.config/nixpkgs/overlays/theming.nix
+++ home/nixpkgs/.config/nixpkgs/overlays/theming.nix
@@ -1,5 +1,7 @@
 self: super:
 
 {
-  yaru-dracula-theme = super.callPackage (builtins.fetchTarball https://github.com/jD91mZM2/yaru-dracula/archive/master.tar.gz) {};
+  # yaru-dracula-theme = super.callPackage (builtins.fetchTarball https://github.com/jD91mZM2/yaru-dracula/archive/master.tar.gz) {};
+  # ^ See https://github.com/ubuntu/yaru/issues/2079
+  yaru-dracula-theme = super.yaru-theme;
 }
