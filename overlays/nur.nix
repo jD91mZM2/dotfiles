3043cabeb222a3b30a63f2420e0834c847ad6d2f
jD91mZM2 <me@krake.one>
2020-08-03 12:14:46 +0200

    Update nur-packages

--- overlays/nur.nix
+++ overlays/nur.nix
@@ -1,11 +1,11 @@
-self: super:
+final: prev:
 
 {
   nur = let
-    nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") { pkgs = self; };
+    nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") { pkgs = final; };
   in nur // {
     repos = nur.repos // {
-      jd91mzm2 = import <dotfiles/nur-packages> { pkgs = self; };
+      jd91mzm2 = import <dotfiles/nur-packages> { pkgs = final; };
     };
   };
 }
