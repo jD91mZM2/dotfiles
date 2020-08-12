c31d2083db0b60935525fd6e2694eb4e34e62a9d
jD91mZM2 <me@krake.one>
2020-08-12 11:13:03 +0200

    Doom Emacs: Add flake.nix template

--- home/generic/doom-config/config/template.el
+++ home/generic/doom-config/config/template.el
@@ -1,4 +1,5 @@
 ;;; templates.el -*- lexical-binding: t; -*-
 
 (set-file-templates!
- '("/\\.gitlab-ci\\.yml$" :trigger "__gitlab_ci_yml" :mode yaml-mode))
+ '("/\\.gitlab-ci\\.yml$" :trigger "__gitlab_ci_yml" :mode yaml-mode)
+ '("/flake\\.nix$" :trigger "__flake_nix" :mode nix-mode))
