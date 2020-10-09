75114aa28fd09014ef059a163e29c5e39c637def
jD91mZM2 <me@krake.one>
2020-10-09 16:20:37 +0200

    Broken elm template

--- home/generic/doom-config/config/template.el
+++ home/generic/doom-config/config/template.el
@@ -4,4 +4,5 @@
  '("/\\.gitlab-ci\\.yml$" :trigger "__gitlab_ci_yml" :mode yaml-mode)
  '("/flake\\.nix$" :trigger "__flake_nix" :mode nix-mode)
  '("\\.desktop$" :trigger "__desktop" :mode conf-desktop-mode)
- '("\\.tex$" :trigger "__tex" :mode latex-mode))
+ '("\\.tex$" :trigger "__tex" :mode latex-mode)
+ '("\\.elm$" :trigger "__elm" :mode elm-mode))
