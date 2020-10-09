1db6afca03a62af672cd6733ef7ac591fa0b4f65
jD91mZM2 <me@krake.one>
2020-10-09 18:15:57 +0200

    Fetch scaff templates dynamically from TOML

--- home/generic/misc.nix
+++ home/generic/misc.nix
@@ -6,19 +6,11 @@ in
   programs.scaff = {
     enable = true;
     imports = let
-      baseURL = "https://gitlab.com/jD91mZM2/scaff-repo/-/jobs/512194133/artifacts/raw/build";
-    in {
-      build-rust-package = builtins.fetchurl "${baseURL}/build-rust-package.tar.gz";
-      cachix             = builtins.fetchurl "${baseURL}/cachix.tar.gz";
-      crate2nix          = builtins.fetchurl "${baseURL}/crate2nix.tar.gz";
-      editorconfig       = builtins.fetchurl "${baseURL}/editorconfig.tar.gz";
-      license-mit        = builtins.fetchurl "${baseURL}/license-mit.tar.gz";
-      naersk             = builtins.fetchurl "${baseURL}/naersk.tar.gz";
-      nix-shell          = builtins.fetchurl "${baseURL}/nix-shell.tar.gz";
-      nixpkgs-rust       = builtins.fetchurl "${baseURL}/nixpkgs-rust.tar.gz";
-      readme             = builtins.fetchurl "${baseURL}/readme.tar.gz";
-      rustfmt            = builtins.fetchurl "${baseURL}/rustfmt.tar.gz";
-    };
+      baseURL = "https://gitlab.com/jD91mZM2/scaff-repo/-/jobs/782984841/artifacts/raw/build";
+      config = builtins.fromTOML (builtins.readFile (builtins.fetchurl "${baseURL}/config.toml"));
+    in pkgs.lib.mapAttrs (_: value:
+      builtins.fetchurl "${baseURL}/${value}"
+    ) config.imports;
   };
 
   programs.mpv = {
