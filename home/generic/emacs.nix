4445734724b3419a1a96828ef9deb978ff782d32
jD91mZM2 <me@krake.one>
2020-10-26 17:31:39 +0100

    Better emacs override

--- home/generic/emacs.nix
+++ home/generic/emacs.nix
@@ -1,26 +1,20 @@
 { config, lib, pkgs, ... }:
 
 let
-  emacs-bin = pkgs.writeShellScriptBin "emacs" ''
+  setup-doom = pkgs.writeShellScript "setup-doom" ''
     dir="''${XDG_CONFIG_HOME:-$HOME/.config}/emacs"
     if [ ! -e "$dir" ]; then
       ${pkgs.git}/bin/git clone https://github.com/hlissner/doom-emacs.git "$dir"
       "$dir/bin/doom" install --no-env --no-config --no-fonts
     fi
-
-    ${pkgs.emacsUnstable}/bin/emacs "$@"
   '';
-  emacs = pkgs.symlinkJoin {
-    name = "emacs";
-    paths = [
-      emacs-bin
-      (pkgs.makeDesktopItem {
-        name = "emacs";
-        desktopName = "Emacs";
-        exec = "${emacs-bin}/bin/emacs";
-      })
-    ];
-  };
+  emacs = pkgs.runCommand "emacs" {
+    nativeBuildInputs = with pkgs; [ makeWrapper ];
+  } ''
+    cp -r "${pkgs.emacsUnstable}" "$out"
+    chmod +w "$out/bin"
+    wrapProgram "$out/bin/emacs" --run "${setup-doom}"
+  '';
 in
 {
   xdg.configFile = {
