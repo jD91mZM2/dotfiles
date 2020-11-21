b58dc5156aef602ea54772fd048fc57a356a0c69
jD91mZM2 <me@krake.one>
2020-11-21 08:38:46 +0100

    Add GitHub PR alias

--- home/generic/shells.nix
+++ home/generic/shells.nix
@@ -60,6 +60,7 @@ in {
 
         aliases = {
           mr = "!f() { git fetch \${2-origin} merge-requests/\${1?}/head && git switch -d FETCH_HEAD; }; f";
+          pr = "!f() { git fetch \${2-origin} pull/\${1?}/head && git switch -d FETCH_HEAD; }; f";
         };
         extraConfig = {
           pull.rebase = true;
