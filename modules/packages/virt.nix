e937f1d3bb05c847f8ac0d91dd9b97ff55123260
jD91mZM2 <me@krake.one>
2021-02-26 17:54:10 +0100

    Re-enable podman

--- modules/packages/virt.nix
+++ modules/packages/virt.nix
@@ -1,7 +1,7 @@
 { pkgs, self, system, ... }:
 {
   # Install podman for containers
-  # virtualisation.podman.enable = true;
+  virtualisation.podman.enable = true;
 
   # Install qemu VM
   environment.systemPackages = with pkgs; [
