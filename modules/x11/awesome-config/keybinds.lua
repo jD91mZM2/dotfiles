f9de4ae42de2685600d5737409f31410a0a92aca
jD91mZM2 <me@krake.one>
2021-03-15 11:03:06 +0100

    awesomewm: formatting i guess

--- modules/x11/awesome-config/keybinds.lua
+++ modules/x11/awesome-config/keybinds.lua
@@ -29,60 +29,197 @@ end
 
 -- Global keybindings
 local globalkeys = gears.table.join(
-  awful.key({ modkey }, "s", hotkeys_popup.show_help, { description = "show help", group = "awesome" }),
+  awful.key(
+    { modkey }, "s",
+    hotkeys_popup.show_help,
+    { description = "show help", group = "awesome" }
+  ),
 
-  awful.key({ modkey }, "j", function () awful.client.focus.byidx( 1) end, { description = "focus next by index", group = "client" }),
-  awful.key({ modkey }, "k", function () awful.client.focus.byidx(-1) end, { description = "focus previous by index", group = "client" }),
+  awful.key(
+    { modkey }, "j", function ()
+      awful.client.focus.byidx( 1)
+    end,
+    { description = "focus next by index", group = "client" }
+  ),
+  awful.key(
+    { modkey }, "k", function ()
+      awful.client.focus.byidx(-1)
+    end,
+    { description = "focus previous by index", group = "client" }
+  ),
 
   -- Layout manipulation
-  awful.key({ modkey, "Shift" }, "j", function () awful.client.swap.byidx(  1) end, { description = "swap with next client by index", group = "client" }),
-  awful.key({ modkey, "Shift" }, "k", function () awful.client.swap.byidx( -1) end, { description = "swap with previous client by index", group = "client" }),
-  awful.key({ modkey, "Control" }, "j", function () awful.screen.focus_relative( 1) end, { description = "focus the next screen", group = "screen" }),
-  awful.key({ modkey, "Control" }, "k", function () awful.screen.focus_relative(-1) end, { description = "focus the previous screen", group = "screen" }),
-  awful.key({ modkey }, "u", awful.client.urgent.jumpto, { description = "jump to urgent client", group = "client" }),
+  awful.key(
+    { modkey, "Shift" }, "j", function ()
+      awful.client.swap.byidx(  1)
+    end,
+    { description = "swap with next client by index", group = "client" }
+  ),
+  awful.key(
+    { modkey, "Shift" }, "k", function ()
+      awful.client.swap.byidx( -1)
+    end,
+    { description = "swap with previous client by index", group = "client" }
+  ),
+  awful.key(
+    { modkey, "Control" }, "j", function ()
+      awful.screen.focus_relative( 1)
+    end,
+    { description = "focus the next screen", group = "screen" }
+  ),
+  awful.key(
+    { modkey, "Control" }, "k", function ()
+      awful.screen.focus_relative(-1)
+    end,
+    { description = "focus the previous screen", group = "screen" }
+  ),
+  awful.key(
+    { modkey }, "u",
+    awful.client.urgent.jumpto,
+    { description = "jump to urgent client", group = "client" }
+  ),
 
   -- Standard program
-  awful.key({ modkey, "Shift" }, "Return", spawn_cmd("st"), { description = "open a terminal", group = "launcher" }),
-  awful.key({ modkey, "Control" }, "r", awesome.restart, { description = "reload awesome", group = "awesome" }),
-  awful.key({ modkey }, "Pause", script("dmenu-confirm.sh Shutdown && systemctl poweroff"), { description = "turn off computer", group = "awesome" }),
-  awful.key({ modkey, "Shift" }, "q", script("dmenu-confirm.sh \"Exit AwesomeWM\" && awesome-client \"awesome.quit()\""), { description = "quit awesome", group = "awesome" }),
+  awful.key(
+    { modkey, "Shift" }, "Return",
+    spawn_cmd("st"),
+    { description = "open a terminal", group = "launcher" }
+  ),
+  awful.key(
+    { modkey, "Control" }, "r",
+    awesome.restart,
+    { description = "reload awesome", group = "awesome" }
+  ),
+  awful.key(
+    { modkey }, "Pause",
+    script("dmenu-confirm.sh Shutdown && systemctl poweroff"),
+    { description = "turn off computer", group = "awesome" }
+  ),
+  awful.key(
+    { modkey, "Shift" }, "q",
+    script("dmenu-confirm.sh \"Exit AwesomeWM\" && awesome-client \"awesome.quit()\""),
+    { description = "quit awesome", group = "awesome" }
+  ),
 
   -- Switch between layouts
-  awful.key({ modkey }, "t", function () awful.layout.set(awful.layout.suit.spiral) end, { description = "tiling view", group = "layout" }),
-  awful.key({ modkey, "Shift" }, "t", function () awful.layout.set(awful.layout.suit.tile) end, { description = "traditional tiling view", group = "layout" }),
-  awful.key({ modkey }, "v", function () awful.layout.set(awful.layout.suit.tile.bottom) end, { description = "tiling view - vertical", group = "layout" }),
-  awful.key({ modkey }, "n", function () awful.layout.set(awful.layout.suit.magnifier) end, { description = "magnifier view", group = "layout" }),
-  awful.key({ modkey }, "m", function () awful.layout.set(awful.layout.suit.max) end, { description = "stacked view", group = "layout" }),
+  awful.key(
+    { modkey }, "t", function ()
+      awful.layout.set(awful.layout.suit.spiral)
+    end,
+    { description = "tiling view", group = "layout" }
+  ),
+  awful.key(
+    { modkey, "Shift" }, "t", function ()
+      awful.layout.set(awful.layout.suit.tile)
+    end,
+    { description = "traditional tiling view", group = "layout" }
+  ),
+  awful.key(
+    { modkey }, "v", function ()
+      awful.layout.set(awful.layout.suit.tile.bottom)
+    end,
+    { description = "tiling view - vertical", group = "layout" }
+  ),
+  awful.key(
+    { modkey }, "n", function ()
+      awful.layout.set(awful.layout.suit.magnifier)
+    end,
+    { description = "magnifier view", group = "layout" }
+  ),
+  awful.key(
+    { modkey }, "m", function ()
+      awful.layout.set(awful.layout.suit.max)
+    end,
+    { description = "stacked view", group = "layout" }
+  ),
 
   -- Misc tag settings
-  awful.key({ modkey }, "b", function () toggle(awful.screen.focused().selected_tag, "display_mono") end, { description = "toggle monocle bar", group = "pertag" }),
-  awful.key({ modkey }, "h", function () awful.tag.incmwfact(-0.05) end, { description = "decrease split ratio", group = "pertag" }),
-  awful.key({ modkey }, "l", function () awful.tag.incmwfact( 0.05) end, { description = "increase split ratio", group = "pertag" }),
-  awful.key({ modkey, "Shift" }, "h", function () awful.screen.focused().selected_tag.master_width_factor = 0.5 end, { description = "reset split ratio", group = "pertag" }),
+  awful.key(
+    { modkey }, "b", function ()
+      toggle(awful.screen.focused().selected_tag, "display_mono")
+    end,
+    { description = "toggle monocle bar", group = "pertag" }
+  ),
+  awful.key(
+    { modkey }, "h", function ()
+      awful.tag.incmwfact(-0.05)
+    end,
+    { description = "decrease split ratio", group = "pertag" }
+  ),
+  awful.key(
+    { modkey }, "l", function ()
+      awful.tag.incmwfact( 0.05)
+    end,
+    { description = "increase split ratio", group = "pertag" }
+  ),
+  awful.key(
+    { modkey, "Shift" }, "h", function ()
+      awful.screen.focused().selected_tag.master_width_factor = 0.5
+    end,
+    { description = "reset split ratio", group = "pertag" }
+  ),
 
   -- Screenshot
-  awful.key({}, "Print", script("screenshot.sh screen"), { description = "dump screen", group = "screenshot" }),
-  awful.key({ modkey }, "Print", script("screenshot.sh window"), { description = "dump window", group = "screenshot" }),
-  awful.key({ modkey, "Shift" }, "Print", script("screenshot.sh region"), { description = "dump region", group = "screenshot" }),
+  awful.key(
+    {}, "Print",
+    script("screenshot.sh screen"),
+    { description = "dump screen", group = "screenshot" }
+  ),
+  awful.key(
+    { modkey }, "Print",
+    script("screenshot.sh window"),
+    { description = "dump window", group = "screenshot" }
+  ),
+  awful.key(
+    { modkey, "Shift" }, "Print",
+    script("screenshot.sh region"),
+    { description = "dump region", group = "screenshot" }
+  ),
 
   -- Toggle touchpad (on my laptop)
-  awful.key({}, "F6", spawn_cmd('xinput enable "$(xinput | grep Touchpad | sed "s/^.*id=\\([0-9]\\+\\).*$/\\1/g")"')),
-  awful.key({}, "F7", spawn_cmd('xinput disable "$(xinput | grep Touchpad | sed "s/^.*id=\\([0-9]\\+\\).*$/\\1/g")"')),
+  awful.key(
+    {}, "F6",
+    spawn_cmd('xinput enable "$(xinput | grep Touchpad | sed "s/^.*id=\\([0-9]\\+\\).*$/\\1/g")"')),
+
+  awful.key(
+    {}, "F7",
+    spawn_cmd('xinput disable "$(xinput | grep Touchpad | sed "s/^.*id=\\([0-9]\\+\\).*$/\\1/g")"')),
+
 
   -- Volume
-  awful.key({}, "F9", spawn_cmd("pactl set-sink-mute \"@DEFAULT_SINK@\" toggle"), { description = "mute/unmute the volume", group = "volume" }),
-  awful.key({}, "F10", script("volume.perl down"), { description = "lower the volume", group = "volume" }),
-  awful.key({}, "F11", script("volume.perl up"), { description = "higher the volume", group = "volume" }),
+  awful.key(
+    {}, "F9",
+    spawn_cmd("pactl set-sink-mute \"@DEFAULT_SINK@\" toggle"),
+    { description = "mute/unmute the volume", group = "volume" }
+  ),
+  awful.key(
+    {}, "F10",
+    script("volume.perl down"),
+    { description = "lower the volume", group = "volume" }
+  ),
+  awful.key(
+    {}, "F11",
+    script("volume.perl up"),
+    { description = "higher the volume", group = "volume" }
+  ),
 
   -- Misc
-  awful.key({ modkey }, "p", script("dmenu-desktop.sh"), { description = "show the menubar", group = "misc" }),
+  awful.key(
+    { modkey }, "p",
+    script("dmenu-desktop.sh"),
+    { description = "show the menubar", group = "misc" }
+  ),
   awful.key({ modkey, "Shift" }, "l", function ()
     naughty.suspend()
     awful.spawn.easy_async_with_shell("i3lock-fancy -pn", function ()
       naughty.resume()
     end)
   end, { description = "lock screen", group = "misc" }),
-  awful.key({ modkey }, "z", spawn_cmd("echo -n \"\xE2\x80\x8B\" | xclip -sel clip"), { description = "clear the clipboard (zero-width space)", group = "misc" })
+  awful.key(
+    { modkey }, "z",
+    spawn_cmd("echo -n \"\xE2\x80\x8B\" | xclip -sel clip"),
+    { description = "clear the clipboard (zero-width space)", group = "misc" }
+  )
 )
 -- Global mouse-bindings
 local globalbuttons = gears.table.join(
@@ -99,30 +236,32 @@ for i = 1, 9 do
     globalkeys,
 
     -- View tag only.
-    awful.key({ modkey }, "#" .. i + 9,
-      function ()
+    awful.key(
+      { modkey }, "#" .. i + 9, function ()
         local screen = awful.screen.focused()
         local tag = screen.tags[i]
         if tag then
           tag:view_only()
         end
       end,
-      { description = "view tag #"..i, group = "tag" }),
+      { description = "view tag #"..i, group = "tag" }
+    ),
 
     -- Toggle tag display.
-    awful.key({ modkey, "Control" }, "#" .. i + 9,
-      function ()
+    awful.key(
+      { modkey, "Control" }, "#" .. i + 9, function ()
         local screen = awful.screen.focused()
         local tag = screen.tags[i]
         if tag then
           awful.tag.viewtoggle(tag)
         end
       end,
-      { description = "toggle tag #" .. i, group = "tag" }),
+      { description = "toggle tag #" .. i, group = "tag" }
+    ),
 
     -- Move client to tag.
-    awful.key({ modkey, "Shift" }, "#" .. i + 9,
-      function ()
+    awful.key(
+      { modkey, "Shift" }, "#" .. i + 9, function ()
         if client.focus then
           local tag = client.focus.screen.tags[i]
           if tag then
@@ -130,11 +269,12 @@ for i = 1, 9 do
           end
         end
       end,
-      { description = "move focused client to tag #"..i, group = "tag" }),
+      { description = "move focused client to tag #"..i, group = "tag" }
+    ),
 
     -- Toggle tag on focused client.
-    awful.key({ modkey, "Control", "Shift" }, "#" .. i + 9,
-      function ()
+    awful.key(
+      { modkey, "Control", "Shift" }, "#" .. i + 9, function ()
         if client.focus then
           local tag = client.focus.screen.tags[i]
           if tag then
@@ -142,22 +282,43 @@ for i = 1, 9 do
           end
         end
       end,
-      { description = "toggle focused client on tag #" .. i, group = "tag" })
+      { description = "toggle focused client on tag #" .. i, group = "tag" }
+    )
   )
 end
 
 -- Window-specific keybindings
 local clientkeys = gears.table.join(
-  awful.key({ modkey }, "f",
-    function (c)
+  awful.key(
+    { modkey }, "f", function (c)
       c.fullscreen = not c.fullscreen
       c:raise()
     end,
-    { description = "toggle fullscreen", group = "client" }),
-  awful.key({ modkey, "Shift" }, "c", function (c) c:kill() end, { description = "close", group = "client" }),
-  awful.key({ modkey, "Control" }, "space", awful.client.floating.toggle, { description = "toggle floating", group = "client" }),
-  awful.key({ modkey }, "Return", function (c) c:swap(awful.client.getmaster()) end, { description = "move to master", group = "client" }),
-  awful.key({ modkey }, "o", function (c) c:move_to_screen() end, { description = "move to screen", group = "client" })
+    { description = "toggle fullscreen", group = "client" }
+  ),
+  awful.key(
+    { modkey, "Shift" }, "c", function (c)
+      c:kill()
+    end,
+    { description = "close", group = "client" }
+  ),
+  awful.key(
+    { modkey, "Control" }, "space",
+    awful.client.floating.toggle,
+    { description = "toggle floating", group = "client" }
+  ),
+  awful.key(
+    { modkey }, "Return", function (c)
+      c:swap(awful.client.getmaster())
+    end,
+    { description = "move to master", group = "client" }
+  ),
+  awful.key(
+    { modkey }, "o", function (c)
+      c:move_to_screen()
+    end,
+    { description = "move to screen", group = "client" }
+  )
 )
 
 -- Window-specific mouse-bindings
